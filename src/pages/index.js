7e85a69384f4591096743e100dc13b22beb60c66
taylorsmithgg <tsmith@spireon.com>
2021-05-24 01:40:41 -0700

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,25 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Typist from 'react-typist'
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div style={{width: '100vh', margin: '0 auto'}}>
+        <img src="./logo.svg" style={{float: 'left'}}/>
+        <div class="terminal" style={{float: 'right'}}>
+          <Typist>
+          <span class="dollar"/> Welcome to SMPL Cloud.<br/>
+          </Typist>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
