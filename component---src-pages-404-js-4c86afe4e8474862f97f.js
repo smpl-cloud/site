e3dc4a0b69afbd8c8b6c0e53f387b89a824dd2ec
taylorsmithgg <tsmith@spireon.com>
2021-05-24 01:54:29 -0700

    Updates

---
+++ component---src-pages-404-js-4c86afe4e8474862f97f.js
@@ -0,0 +1,2 @@
+(self.webpackChunksmpl_cloud=self.webpackChunksmpl_cloud||[]).push([[883],{9616:function(e,t,l){"use strict";l.r(t);var n=l(7294),u=l(7198),s=l(3751);t.default=function(){return n.createElement(u.Z,null,n.createElement(s.Z,{title:"404: Not found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4c86afe4e8474862f97f.js.map
